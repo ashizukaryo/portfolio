5f2b923d3ae96735833c60cbab5d1e6d370a2cb4
ashizukaryo <nekonekokingkids@gmail.com>
2021-07-24 20:12:30 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var scrollAnimationElm = document.querySelectorAll('.douga1');
 var scrollAnimationFunc = function() {
   for(var i = 0; i < scrollAnimationElm.length; i++) {
-    var triggerMargin = 300;
+    var triggerMargin = 400;
     if (window.innerHeight > scrollAnimationElm[i].getBoundingClientRect().top + triggerMargin) {
       scrollAnimationElm[i].classList.add('show');
     }
